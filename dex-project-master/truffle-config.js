d3bd7a7c5567d63e15aca82b220e0b5033168baf
victorlfreire <victorlfreire@icloud.com>
2021-11-23 21:38:38 -0500

    update

---
+++ dex-project-master/truffle-config.js
@@ -0,0 +1,37 @@
+require('babel-register');
+require('babel-polyfill');
+require('dotenv').config();
+const HDWalletprovider = require('truffle-hdwallet-provider-privkey');
+const privateKeys = process.env.PRIVATE_KEYS || ""
+
+module.exports = {
+  networks: {
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
+    kovan: {
+      provider: function() {
+        return new HDWalletprovider(
+          privateKeys.split(','),
+          'https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}'
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 42
+      
+    }
+  },
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  compilers: {
+    solc: {
+       optimizer: {
+         enabled: true,
+         runs: 200
+       }
+    }
+  }
+}
